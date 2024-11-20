d0d1e8d4e71ffb9a541250a03f92a942056cd1da
shenranw <wsr.20021128@gmail.com>
2024-11-19 22:10:55 -0800

    add chop-off for exp

--- sae_analysis.py
+++ sae_analysis.py
@@ -7,13 +7,15 @@ MODEL = os.getenv("MODEL")
 DATASET = os.getenv("DATASET")
 INDEX = int(os.getenv("INDEX"))
 DATA_PATH = f"./experimental_data/{MODEL}/{DATASET}/"
-CHOP_OFF = int(os.getenv("CHOP_OFF"))
+CHOP_OFF = int(os.getenv("CHOP_OFF", -1))
+CHOP_OFF_EXP = int(os.getenv("CHOP_OFF_EXP", -1))
 STREAM = os.getenv("STREAM", "res")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 acts_resid = torch.load(DATA_PATH + f"acts_resid_generation_{INDEX}.pt", map_location=device)
 acts_resid = acts_resid[:CHOP_OFF, :, :]
 acts_exp_resid = torch.load(DATA_PATH + f"acts_exp_resid_generation_{INDEX}.pt", map_location=device)
+acts_exp_resid = acts_exp_resid[:CHOP_OFF_EXP, :, :]
 
 acts_resid_args = top_k_abs_acts_args(acts_resid, 1)      # (samples, layers, 1)
 acts_exp_resid_args = top_k_abs_acts_args(acts_exp_resid, 1)
