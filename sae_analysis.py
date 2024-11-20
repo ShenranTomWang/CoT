0b0915fa55d3d18192c4f6d20f08dd6ea1739781
shenranw <wsr.20021128@gmail.com>
2024-11-19 17:22:14 -0800

    save filename with index

--- sae_analysis.py
+++ sae_analysis.py
@@ -21,8 +21,8 @@ acts_exp_resid_args = top_k_abs_acts_args(acts_exp_resid, 1)
 resid_desc = get_args_desc(MODEL, STREAM, acts_resid_args)
 resid_exp_desc = get_args_desc(MODEL, STREAM, acts_exp_resid_args)
 
-with open(DATA_PATH + "acts_exp_desc.json", "w") as f:
+with open(DATA_PATH + f"acts_exp_desc_{INDEX}.json", "w") as f:
     json.dump(resid_exp_desc, f, indent=4)
     
-with open(DATA_PATH + "acts_desc.json", "w") as f:
+with open(DATA_PATH + f"acts_desc_{INDEX}.json", "w") as f:
     json.dump(resid_desc, f, indent=4)
