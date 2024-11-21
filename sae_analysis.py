a5387846c6309d8b6107c85c88fef7b9135f0b23
shenranw <wsr.20021128@gmail.com>
2024-11-20 20:02:56 -0800

    debug

--- sae_analysis.py
+++ sae_analysis.py
@@ -5,7 +5,7 @@ from utils.analysis_utils import top_k_abs_acts_args, get_args_desc
 
 MODEL = os.getenv("MODEL")
 DATASET = os.getenv("DATASET")
-INDEX = int(os.getenv("INDEX, 0"))
+INDEX = int(os.getenv("INDEX", 0))
 DATA_PATH = f"./experimental_data/{MODEL}/{DATASET}/"
 CHOP_OFF = int(os.getenv("CHOP_OFF", -1))
 CHOP_OFF_EXP = int(os.getenv("CHOP_OFF_EXP", -1))
@@ -19,7 +19,7 @@ acts_resid = acts_resid[:CHOP_OFF, :, :]
 acts_exp_resid = torch.load(DATA_PATH + f"acts_exp_resid_generation_{INDEX}.pt", map_location=device)
 acts_exp_resid = acts_exp_resid[:CHOP_OFF_EXP, :, :]
 
-acts_resid_args = top_k_abs_acts_args(acts_resid, TOP_K, layer=LAYER)      # (samples, layers, 1)
+acts_resid_args = top_k_abs_acts_args(acts_resid, TOP_K, layer=LAYER)      # (samples, layers, TOP_K)
 acts_exp_resid_args = top_k_abs_acts_args(acts_exp_resid, TOP_K, layer=LAYER)
 
 resid_desc = get_args_desc(MODEL, STREAM, acts_resid_args)
