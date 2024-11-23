aa86a663b541d84e978a94785496c28dc175745f
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-23 15:09:43 -0500

    turn off verbose loading by default

--- utils/loading_utils.py
+++ utils/loading_utils.py
@@ -57,10 +57,10 @@ def load_data(n_shots: int, data_path: str, data_key: str = "examples", lookup_k
 
     return train, test, pair_id_lookup
 
-def load_model(model_name: str, device='cpu', dtype=torch.bfloat16) -> HookedTransformer:
+def load_model(model_name: str, device='cpu', dtype=torch.bfloat16, verbose=False) -> HookedTransformer:
     print(f"Loading model {model_name}...")
     weights_directory = "./models/" + config_model[model_name]['weights_directory']
-    model = get_pretrained_model(weights_directory, dtype=dtype, device=device, verbose=True)
+    model = get_pretrained_model(weights_directory, dtype=dtype, device=device, verbose=verbose)
     return model
 
 def load_tokenizer(model_name: str, device='cpu') -> AutoTokenizer:
