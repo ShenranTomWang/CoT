3eba0f813d62540d53a685ed839c62570b2a904e
shenranw <wsr.20021128@gmail.com>
2024-11-27 20:45:17 -0800

    centered diff graph

--- utils/plotting_utils.py
+++ utils/plotting_utils.py
@@ -18,18 +18,19 @@ def draw_heatmap(tensor: torch.Tensor, title: str, axis=None):
     ax.set_ylabel('Layer')
     ax.set_title(title)
     
-def draw_attention(attention_weights: torch.Tensor,  title: str, axis=None, cmap='viridis', tokens=False):
+def draw_attention(attention_weights: torch.Tensor,  title: str, axis=None, cmap='viridis', tokens=False, center=None):
     """
     Draws a heatmap of the self-attention weights with token descriptions.
     args:
         attention_weights: The (seqlen_q, seqlen_k) matrix of self-attention weights.
         tokens: The list of token descriptions. Default to False if not needed.
         title: The title of the heatmap.
-        axis: The axis to draw the heatmap on (for side-by-side plots), default to None if not needed
-        cmap: The colormap to use for the heatmap, default to 'viridis'
+        axis: The axis to draw the heatmap on (for side-by-side plots), default to None if not needed.
+        cmap: The colormap to use for the heatmap, default to 'viridis'.
+        center: The center value of the colormap, default to None.
     """
     data = attention_weights.transpose(0, 1).cpu().numpy()
-    sns.heatmap(data, cmap=cmap, cbar=True, xticklabels=tokens, yticklabels=tokens, ax=axis)
+    sns.heatmap(data, cmap=cmap, cbar=True, xticklabels=tokens, yticklabels=tokens, ax=axis, center=center)
 
     ax = axis if axis is not None else plt.gca()
     ax.set_xlabel('Query Tokens')
